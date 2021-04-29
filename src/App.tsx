4e409c9b12654fac2989fc71f2562cd921c82183
CareteenL <15074806497@163.com>
2021-04-29 10:11:51 +0800

    feat: 🎸 imp simple drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,77 @@
-import React from 'react'
-import Counter from './packages/Counter'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef, useState } from 'react'
 
 function App() {
+  const [pos, setPos] = useState({
+    top: 0,
+    left: 0,
+  })
+
+  const posRef = useRef(pos)
+  posRef.current = pos
+
+  const moveDraggier = (() => {
+    const dragData = useRef({
+      startTop: 0,
+      startLeft: 0,
+      startX: 0,
+      startY: 0,
+    })
+    const mousedown = (e: React.MouseEvent<HTMLElement>) => {
+      document.addEventListener('mousemove', mousemove)
+      document.addEventListener('mouseup', mouseup)
+      dragData.current = {
+        startLeft: pos.left,
+        startTop: pos.top,
+        startX: e.clientX,
+        startY: e.clientY,
+      }
+    }
+    const mousemove = (e: MouseEvent) => {
+      // console.log(JSON.stringify(pos))
+      console.log({
+        pos: `${pos.left}_${pos.top}`,
+        ref: `${posRef.current.left}_${posRef.current.top}`,
+      })
+      const {
+        startLeft,
+        startTop,
+        startX,
+        startY,
+      } = dragData.current
+      const durX = e.clientX - startX
+      const durY = e.clientY - startY
+      setPos({
+        top: startTop + durY,
+        left: startLeft + durX,
+      })
+    }
+    const mouseup = (e: MouseEvent) => {
+      document.removeEventListener('mousemove', mousemove)
+      document.removeEventListener('mouseup', mouseup)      
+    }
+    return {
+      mousedown,
+    }
+  })()
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <Counter />
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="app-home">
+      <div style={{
+        width: '50px',
+        height: '50px',
+        backgroundColor: 'green',
+        position: 'relative',
+        top: `${pos.top}px`,
+        left: `${pos.left}px`,
+        display: 'inline-block',
+        color: '#fff',
+        fontSize: '18px',
+        cursor: 'move',
+      }}
+      onMouseDown={moveDraggier.mousedown}
+      >
+        快拖拽我
+      </div>
     </div>
   )
 }
